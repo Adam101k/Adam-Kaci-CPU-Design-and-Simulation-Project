18984d24b2bc658591d1aae3d3158ccc1cdc9728
Adam101k <adamkaci3@csu.fullerton.edu>
2025-11-22 07:48:04 -0800

    Carrying Over old ALU FPU work
    In a different group I had written the following scripts already by myself. I will have to write the "from memory import..." part of alu.py myself and any additional dependencies that are required

---
+++ src/alu.py
@@ -0,0 +1,25 @@
+from memory import Bit, Bitx12, Bitx32
+
+class ALU:
+    def __init__(self):
+        pass
+    
+    def execute(self, opcode:str, input1, input2, input3):
+        if opcode == "add":
+            pass
+        elif opcode == "sub":
+            pass
+        elif opcode == "and":
+            pass
+        elif opcode == "or":
+            pass
+        elif opcode == "xor":
+            pass
+        elif opcode == "sll":
+            pass
+        elif opcode == "srl":
+            pass
+        elif opcode == "sra":
+            pass
+        else:
+            pass
